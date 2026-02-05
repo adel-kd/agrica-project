fbfd603dd3dde10666789920a3a737d7a0b1c68b
adel Kedir <adelkedir971@gmail.com>
2026-02-05 23:49:58 +0300

    Delete backend directory

--- backend/src/routes/ai.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { processMessage } = require("../controllers/ai.controller");
-
-router.post("/ask", processMessage);
-
-module.exports = router;
