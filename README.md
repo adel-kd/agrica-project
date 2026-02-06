7f8ea5969fe696b656721333e8126cbdf879da7f
Amanuel100 <amanueleyobshiferew@gmail.com>
2026-02-06 09:18:06 +0200

    Remove duplicated nested agrica-project folder

--- README.md
+++
@@ -1,1 +0,0 @@
-# agrica-project
--- agrica-project/backend/src/utilis/logger.js
+++
@@ -1,9 +0,0 @@
-const logInfo = (message, meta = {}) => {
-  console.log(`[INFO] ${message}`, meta);
-};
-
-const logError = (message, meta = {}) => {
-  console.error(`[ERROR] ${message}`, meta);
-};
-
-module.exports = { logInfo, logError };
